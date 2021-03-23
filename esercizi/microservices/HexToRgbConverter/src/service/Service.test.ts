63158cc48ab9c72f32afd2f839fe1eb6df840be5
Paolo Invernizzi <invernp91@gmail.com>
2021-03-23 15:13:05 +0100

    Implement service and service test

--- esercizi/microservices/HexToRgbConverter/src/service/Service.test.ts
+++ esercizi/microservices/HexToRgbConverter/src/service/Service.test.ts
@@ -1,13 +1,14 @@
 import chai from 'chai';
 import {convert} from './Service';
+import {hex2rgbTestData} from "../../../commons/src/test-data/colors";
 
 chai.config.includeStack = true;
 const should = chai.should();
 
 describe('test suite description', () => {
-    testData.forEach((test) => {
-        it(`test case description`, () => {
-            convert(test.input).should.deep.equal(test.expected);
+    hex2rgbTestData.forEach((test) => {
+        it(`Should convert Hex value to corresponding RGB value`, () => {
+            convert(test.hexValue).should.deep.equal(test.rgbValue);
         });
     });
 });
--- esercizi/microservices/HexToRgbConverter/src/service/Service.ts
+++ esercizi/microservices/HexToRgbConverter/src/service/Service.ts
@@ -1,7 +1,9 @@
 import colorConverter from 'color-convert'
+import { RGB } from 'color-convert/conversions';
+import {TtfHex, TtfRgb} from "../../../commons/src/model/Color";
 
-export function convert(color: ColorModel): ColorModel {
-    const colorToConvert: ColorModel = {};
-    const convertedColor = colorConverter;
-    return {} as ColorModel;
+export function convert(color: TtfHex): TtfRgb {
+    const colorToConvert:TtfHex = color;
+    const convertedColor:RGB = colorConverter.hex.rgb(color.hex); // [ 255, 50, 240]
+    return {red: convertedColor[0], green: convertedColor[1], blue: convertedColor[2]} as TtfRgb;
 }
