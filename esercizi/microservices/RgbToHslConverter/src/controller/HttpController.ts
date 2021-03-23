1612b9ec95408bda3adec89a403d2e636edc9639
Mariacristina Accettura <mcristinaccettura@gmail.com>
2021-03-23 15:43:48 +0100

    Implement RgbToHsl controller and controller test

--- esercizi/microservices/RgbToHslConverter/src/controller/HttpController.ts
+++ esercizi/microservices/RgbToHslConverter/src/controller/HttpController.ts
@@ -1,15 +1,16 @@
-import {convert} from '../service/Service';
-import {Express} from 'express';
+import { convert } from "../service/Service";
+import { Express } from "express";
+import { TtfRgb, TtfHsl } from "../../../commons/src/model/Color";
 
 class HttpController {
-    constructor(server: Express) {
-        server.get('/', (req, res) => {
-            const color = JSON.parse(req.query.color) as ColorModel;
-            const convertedColor: ColorModel = convert(color);
+  constructor(server: Express) {
+    server.get("/", (req, res) => {
+      const color = JSON.parse(req.query.color as string) as TtfRgb;
+      const convertedColor: TtfHsl = convert(color);
 
-            res.send(convertedColor);
-        });
-    }
+      res.send(convertedColor);
+    });
+  }
 }
 
 export default HttpController;
