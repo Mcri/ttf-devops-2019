40581d9ea580f459cfc5c74b9b5cebb7d5cd57d7
Mariacristina Accettura <mcristinaccettura@gmail.com>
2021-03-25 18:56:52 +0100

    Implement HslToRgb service and service test

--- esercizi/microservices/HslToRgbConverter/src/service/Service.ts
+++ esercizi/microservices/HslToRgbConverter/src/service/Service.ts
@@ -1,7 +1,13 @@
-import colorConverter from 'color-convert'
+import colorConverter from "color-convert";
+import { HSL } from "color-convert/conversions";
+import { TtfHsl, TtfRgb } from "../../../commons/src/model/Color";
 
-export function convert(color: ColorModel): ColorModel {
-    const colorToConvert: ColorModel = {};
-    const convertedColor = colorConverter;
-    return {} as ColorModel;
+export function convert(color: TtfHsl): TtfRgb {
+  const colorToConvert: HSL = [color.hue, color.saturation, color.lightness];
+  const convertedColor = colorConverter.hsl.rgb(colorToConvert);
+  return {
+    red: convertedColor[0],
+    green: convertedColor[1],
+    blue: convertedColor[2],
+  } as TtfRgb;
 }
