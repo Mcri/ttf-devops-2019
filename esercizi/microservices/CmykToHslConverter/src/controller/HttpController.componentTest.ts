543500c6a899fa742a6c37f1d31058437552a6cc
Paolo Invernizzi <invernp91@gmail.com>
2021-03-25 15:40:25 +0100

    Implement CmykToHsl HTTP controller and controller test

--- esercizi/microservices/CmykToHslConverter/src/controller/HttpController.componentTest.ts
+++ esercizi/microservices/CmykToHslConverter/src/controller/HttpController.componentTest.ts
@@ -1,24 +1,26 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import * as config from '../../server-config.json'
+import { hsl2cmykTestData } from '../../../commons/src/test-data/colors';
 
 chai.config.includeStack = true;
 const should = chai.should();
 chai.use(chaiHttp);
 
-describe('REST API test suite description', () => {
+describe('Test CmykToHsl REST API', () => {
     const url = process.env.npm_config_rgb2hex_test_url || `http://localhost:${config.port}`;
     console.log('Test URL: ' + url);
 
-    testData.forEach((test) => {
-        it(`test case description`, (done) => {
+    hsl2cmykTestData.forEach((test) => {
+
+        it(`Should include in the response body the HSL value corresponding to the query: ${test.cmykValue}`, (done) => {
             chai.request(url)
                 .get('/')
-                .query(`color=${JSON.stringify(test.input)}`)
+                .query(`color=${JSON.stringify(test.cmykValue)}`)
                 .end((err, res) => {
                     should.not.exist(err);
                     res.should.have.status(200);
-                    res.body.should.deep.equal(test.expected);
+                    res.body.should.deep.equal(test.hslValue);
                     done();
                 });
         });
--- esercizi/microservices/CmykToHslConverter/src/controller/HttpController.ts
+++ esercizi/microservices/CmykToHslConverter/src/controller/HttpController.ts
@@ -1,11 +1,12 @@
 import {convert} from '../service/Service';
 import {Express} from 'express';
+import { TtfCmyk, TtfHsl } from '../../../commons/src/model/Color';
 
 class HttpController {
     constructor(server: Express) {
         server.get('/', (req, res) => {
-            const color = JSON.parse(req.query.color) as ColorModel;
-            const convertedColor: ColorModel = convert(color);
+            const color = JSON.parse(req.query.color as string) as TtfCmyk;
+            const convertedColor: TtfHsl = convert(color);
 
             res.send(convertedColor);
         });
