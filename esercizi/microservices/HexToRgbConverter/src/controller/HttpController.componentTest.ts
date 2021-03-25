10f8e31c7c21a4daa804159de502f61796c485d1
Mariacristina Accettura <mcristinaccettura@gmail.com>
2021-03-25 18:20:43 +0100

    Update HexToRgb tests

--- esercizi/microservices/HexToRgbConverter/src/controller/HttpController.componentTest.ts
+++ esercizi/microservices/HexToRgbConverter/src/controller/HttpController.componentTest.ts
@@ -1,27 +1,60 @@
-import chai from 'chai';
-import chaiHttp from 'chai-http';
-import * as config from '../../server-config.json'
-import {hex2rgbTestData} from "../../../commons/src/test-data/colors";
+import chai from "chai";
+import chaiHttp from "chai-http";
+import * as config from "../../server-config.json";
+import { hex2rgbTestData } from "../../../commons/src/test-data/colors";
 
 chai.config.includeStack = true;
 const should = chai.should();
 chai.use(chaiHttp);
 
-describe('Test HexToRgbConverter REST API', () => {
-    const url = process.env.npm_config_rgb2hex_test_url || `http://localhost:${config.port}`;
-    console.log('Test URL: ' + url);
+describe("Test HexToRgbConverter REST API", () => {
+  const url =
+    process.env.npm_config_rgb2hex_test_url ||
+    `http://localhost:${config.port}`;
+  console.log("Test URL: " + url);
 
-    hex2rgbTestData.forEach((test) => {
-        it(`Should include in the response body the RGB value corresponding to the query: ${test.hexValue.hex}`, (done) => {
-            chai.request(url)
-                .get('/')
-                .query(`color=${JSON.stringify(test.hexValue)}`)
-                .end((err, res) => {
-                    should.not.exist(err);
-                    res.should.have.status(200);
-                    res.body.should.deep.equal(test.rgbValue);
-                    done();
-                });
+  hex2rgbTestData.forEach((test) => {
+    it(`Should include in the response body the RGB value corresponding to the query: ${test.hexValue.hex}`, (done) => {
+      chai
+        .request(url)
+        .get("/")
+        .query(`color=${JSON.stringify(test.hexValue)}`)
+        .end((err, res) => {
+          should.not.exist(err);
+          res.should.have.status(200);
+          res.body.should.deep.equal(test.rgbValue);
+          done();
         });
     });
+  });
+
+  it("Should return an error message when no value is provided as query parameter", (done) => {
+    const errMsg =
+      "Error! The correct usage for this service is: localhost:HexToRgbController?color={'hex': <string>}";
+    const errJson = { error: errMsg };
+    chai
+      .request(url)
+      .get("/")
+      .query(`color=`)
+      .end((err, res) => {
+        res.should.have.status(400);
+        res.body.should.deep.equal(errJson);
+        done();
+      });
+  });
+
+  it("Should return an error message when an invalid value is provided as query parameter", (done) => {
+    const errMsg =
+      "Error! The correct usage for this service is: localhost:HexToRgbController?color={'hex': <string>}";
+    const errJson = { error: errMsg };
+    chai
+      .request(url)
+      .get("/")
+      .query(`color="{'hex': 'sf#na&jsfnaf'}"`)
+      .end((err, res) => {
+        res.should.have.status(400);
+        res.body.should.deep.equal(errJson);
+        done();
+      });
+  });
 });
--- esercizi/microservices/HexToRgbConverter/src/controller/HttpController.ts
+++ esercizi/microservices/HexToRgbConverter/src/controller/HttpController.ts
@@ -1,16 +1,23 @@
-import {convert} from '../service/Service';
-import {Express} from 'express';
-import { TtfHex, TtfRgb } from '../../../commons/src/model/Color';
+import { convert } from "../service/Service";
+import { Express } from "express";
+import { TtfHex, TtfRgb } from "../../../commons/src/model/Color";
 
 class HttpController {
-    constructor(server: Express) {
-        server.get('/', (req, res) => {
-            const color:TtfHex = JSON.parse(req.query.color as string) as TtfHex;
-            const convertedColor: TtfRgb = convert(color);
+  constructor(server: Express) {
+    server.get("/", (req, res) => {
+      try {
+        const color: TtfHex = JSON.parse(req.query.color as string) as TtfHex;
+        if (color.hex === undefined) throw new Error();
+        const convertedColor: TtfRgb = convert(color);
 
-            res.send(convertedColor);
-        });
-    }
+        res.send(convertedColor);
+      } catch (err) {
+        const errMsg =
+          "Error! The correct usage for this service is: localhost:HexToRgbController?color={'hex': <string>}";
+        res.status(400).json({ error: errMsg });
+      }
+    });
+  }
 }
 
 export default HttpController;
