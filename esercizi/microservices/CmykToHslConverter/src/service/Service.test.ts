7ff5779ba82878be73be7a0c451507cbfa2e93e0
Paolo Invernizzi <invernp91@gmail.com>
2021-03-25 15:31:50 +0100

    Implement CmykToHsl service and service test

--- esercizi/microservices/CmykToHslConverter/src/service/Service.test.ts
+++ esercizi/microservices/CmykToHslConverter/src/service/Service.test.ts
@@ -1,13 +1,14 @@
 import chai from 'chai';
 import {convert} from './Service';
+import {hsl2cmykTestData} from "../../../commons/src/test-data/colors";
 
 chai.config.includeStack = true;
 const should = chai.should();
 
 describe('test suite description', () => {
-    testData.forEach((test) => {
-        it(`test case description`, () => {
-            convert(test.input).should.deep.equal(test.expected);
+    hsl2cmykTestData.forEach((test) => {
+        it(`Should convert CMYK value to corresponding HSL value`, () => {
+            convert(test.cmykValue).should.deep.equal(test.hslValue);
         });
     });
 });
